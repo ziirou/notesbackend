d06bf6d25d90b5afbba7644b2e315fd1cfe32b19
Tero Nikula <37400796+ziirou@users.noreply.github.com>
2025-03-24 09:54:35 +0200

    Serve static files

--- index.js
+++ index.js
@@ -31,8 +31,9 @@ const requestLogger = (request, response, next) => {
   next()
 }
 
-app.use(express.json())
 app.use(requestLogger)
+app.use(express.json())
+app.use(express.static('dist'))
 
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
