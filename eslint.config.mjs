569366ebc49f8af850c8c2d053a2c8292d3f4774
Tero Nikula <37400796+ziirou@users.noreply.github.com>
2025-03-25 14:42:19 +0200

    Add and configure Lint

---
+++ eslint.config.mjs
@@ -0,0 +1,27 @@
+import globals from 'globals'
+import js from '@eslint/js'
+import stylisticJs from '@stylistic/eslint-plugin-js'
+
+export default [
+  js.configs.recommended,
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      sourceType: 'commonjs',
+      globals: { ...globals.node },
+      ecmaVersion: 'latest',
+    },
+    plugins: { 
+      '@stylistic/js': stylisticJs,
+    },
+    rules: { 
+      '@stylistic/js/indent': ['error', 2],
+      '@stylistic/js/linebreak-style': ['error', 'unix'],
+      '@stylistic/js/quotes': ['error', 'single'],
+      '@stylistic/js/semi': ['error', 'never'],
+    }, 
+  },
+  { 
+    ignores: ['dist/**'], 
+  },
+]
