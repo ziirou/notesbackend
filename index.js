bda32b15c80c64b0c66764759584ebf0e1de1a1b
Tero Nikula <37400796+ziirou@users.noreply.github.com>
2025-03-25 11:33:58 +0200

    Add note validation

--- index.js
+++ index.js
@@ -50,21 +50,19 @@ const setImportance = (important) => {
   return important
 }
 
-app.post('/api/notes', (request, response) => {
+app.post('/api/notes', (request, response, next) => {
   const body = request.body
-
-  if (!body.content) {
-    return response.status(400).json({ error: 'content missing' })
-  }
 
   const note = new Note({
     content: body.content,
     important: setImportance(body.important),
   })
 
-  note.save().then(savedNote => {
-    response.json(savedNote)
-  })
+  note.save()
+    .then(savedNote => {
+      response.json(savedNote)
+    })
+    .catch(error => next(error))
 })
 
 app.put('/api/notes/:id', (request, response, next) => {
@@ -77,7 +75,7 @@ app.put('/api/notes/:id', (request, response, next) => {
       }
 
       note.content = content
-      note.important = important
+      note.important = setImportance(important)
 
       return note.save().then((updatedNote) => {
         response.json(updatedNote)
@@ -105,6 +103,8 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
   }
 
   next(error)
