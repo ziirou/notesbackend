7fabb1e155e4bad37b8d5398aa5dfa83e517e51e
Tero Nikula <37400796+ziirou@users.noreply.github.com>
2025-03-25 09:42:34 +0200

    Add edit note operation

--- index.js
+++ index.js
@@ -67,6 +67,25 @@ app.post('/api/notes', (request, response) => {
   })
 })
 
+app.put('/api/notes/:id', (request, response, next) => {
+  const { content, important } = request.body
+
+  Note.findById(request.params.id)
+    .then(note => {
+      if (!note) {
+        return response.status(404).end()
+      }
+
+      note.content = content
+      note.important = important
+
+      return note.save().then((updatedNote) => {
+        response.json(updatedNote)
+      })
+    })
+    .catch(error => next(error))
+})
+
 app.delete('/api/notes/:id', (request, response) => {
   Note.findByIdAndDelete(request.params.id)
     .then(result => {
