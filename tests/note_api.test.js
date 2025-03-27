89ee4c4a6e57028d6a455da8a0d4f5c29979c45d
Tero Nikula <37400796+ziirou@users.noreply.github.com>
2025-03-27 09:54:11 +0200

    Part 4b: Optimizing the beforeEach function

--- tests/note_api.test.js
+++ tests/note_api.test.js
@@ -9,12 +9,7 @@ const Note = require('../models/note')
 
 beforeEach(async () => {
   await Note.deleteMany({})
-
-  let noteObject = new Note(helper.initialNotes[0])
-  await noteObject.save()
-
-  noteObject = new Note(helper.initialNotes[1])
-  await noteObject.save()
+  await Note.insertMany(helper.initialNotes)
 })
 
 describe('note_api', () => {
