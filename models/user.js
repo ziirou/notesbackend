0c361db3b964bf2abcd58377a7abc21e9799402c
Tero Nikula <37400796+ziirou@users.noreply.github.com>
2025-03-28 10:14:47 +0200

    Part 4c: Creating users
    Trying bcryptjs instead of bcrypt. Installing bcrypt gave deprecation warnings:
    
    npm warn deprecated inflight@1.0.6: This module is not supported, and leaks memory. Do not use it. Check out lru-cache if you want a good and tested way to coalesce async requests by a key value, which is much more comprehensive and powerful.
    npm warn deprecated npmlog@5.0.1: This package is no longer supported.
    npm warn deprecated rimraf@3.0.2: Rimraf versions prior to v4 are no longer supported
    npm warn deprecated glob@7.2.3: Glob versions prior to v9 are no longer supported
    npm warn deprecated are-we-there-yet@2.0.0: This package is no longer supported.
    npm warn deprecated gauge@3.0.2: This package is no longer supported.

--- models/user.js
+++ models/user.js
@@ -1,7 +1,11 @@
 const mongoose = require('mongoose')
 
 const userSchema = mongoose.Schema({
-  username: String,
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
   name: String,
   passwordHash: String,
   notes: [
