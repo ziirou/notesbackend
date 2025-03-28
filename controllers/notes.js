32c083bc2708ec08bc2d4b594bd88c2d23c85fa3
Tero Nikula <37400796+ziirou@users.noreply.github.com>
2025-03-28 14:53:14 +0200

    Part 4d: Limit creating notes to logged-in users

--- controllers/notes.js
+++ controllers/notes.js
@@ -1,6 +1,27 @@
+const jwt = require('jsonwebtoken')
 const notesRouter = require('express').Router()
 const Note = require('../models/note')
 const User = require('../models/user')
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '')
+  }
+  return null
+}
+
+const setImportance = (important) => {
+  if (typeof important !== 'boolean') {
+    if (important === 'false') {
+      return false
+    }
+
+    return true
+  }
+
+  return important
+}
 
 notesRouter.get('/', async (request, response) => {
   const notes = await Note
@@ -18,22 +39,15 @@ notesRouter.get('/:id', async (request, response) => {
   }
 })
 
-const setImportance = (important) => {
-  if (typeof important !== 'boolean') {
-    if (important === 'false') {
-      return false
-    }
-
-    return true
-  }
-
-  return important
-}
-
 notesRouter.post('/', async (request, response) => {
   const body = request.body
 
-  const user = await User.findById(body.userId)
+  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid' })
+  }
+
+  const user = await User.findById(decodedToken.id)
 
   const note = new Note({
     content: body.content,
